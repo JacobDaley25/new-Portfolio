c103540913b9a7c675e187d8e6bc6846159334c4
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-04-12 14:46:43 -0400

    created react app

---
+++ new-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return(
+    <div className='container'>
+      <div className='nameTag'>
+        <h1>Brian "Jacob" Daley</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App;
